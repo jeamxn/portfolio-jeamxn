c4aab53f9442e35b787a968437d0ee0f786bf4bd
jeamxn <admin@chicken-moo.com>
2024-08-29 09:38:12 +0900

    feat: error hand

--- src/app/getProfile.ts
+++ src/app/getProfile.ts
@@ -1,10 +1,15 @@
 import { Client } from "@notionhq/client";
 
 export const getProfile = async () => {
-  const notion = new Client({ auth: process.env.NOTION_API_KEY });
-  const response = await notion.pages.retrieve({
-    page_id: process.env.NOTION_MAIN!,
-  });
-  const teams = (response as any)?.cover?.file.url;
-  return teams;
+  try{
+    const notion = new Client({ auth: process.env.NOTION_API_KEY });
+    const response = await notion.pages.retrieve({
+      page_id: process.env.NOTION_MAIN!,
+    });
+    const teams: string = (response as any)?.cover?.file.url;
+    return teams;
+  }
+  catch{
+    return "";
+  }
 };
--- src/app/getProjects.ts
+++ src/app/getProjects.ts
@@ -1,23 +1,28 @@
 import { Client } from "@notionhq/client";
 
 export const getProjects = async () => {
-  const notion = new Client({ auth: process.env.NOTION_API_KEY });
-  const response = await notion.databases.query({
-    database_id: process.env.NOTION_DATABASE_PROJECTS!,
-  });
-  const projects = response.results.map((page) => {
-    return {
-      icon: (page as any).icon?.file.url,
-      cover: (page as any).cover?.file.url,
-      priority: (page as any).properties.priority.number,
-      url: (page as any).public_url,
-      data: {
-        title: (page as any).properties.name.title[0].plain_text,
-        description: (page as any).properties.description.rich_text[0].plain_text,
-        startDate: (page as any).properties.start.date?.start || "",
-        endDate: (page as any).properties.end.date?.start || "",
-      }
-    };
-  });
-  return projects.sort((a, b) => b.priority - a.priority);
+  try{
+    const notion = new Client({ auth: process.env.NOTION_API_KEY });
+    const response = await notion.databases.query({
+      database_id: process.env.NOTION_DATABASE_PROJECTS!,
+    });
+    const projects = response.results.map((page) => {
+      return {
+        icon: (page as any).icon?.file.url,
+        cover: (page as any).cover?.file.url,
+        priority: (page as any).properties.priority.number,
+        url: (page as any).public_url,
+        data: {
+          title: (page as any).properties.name.title[0].plain_text,
+          description: (page as any).properties.description.rich_text[0].plain_text,
+          startDate: (page as any).properties.start.date?.start || "",
+          endDate: (page as any).properties.end.date?.start || "",
+        }
+      };
+    });
+    return projects.sort((a, b) => b.priority - a.priority);
+  }
+  catch {
+    return [];
+  }
 };
