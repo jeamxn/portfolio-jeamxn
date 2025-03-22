8f40325baa5455ee9e84021ed6f242eb55326f6d
jeamxn <admin@chicken-moo.com>
2025-03-22 15:04:30 +0900

    fix: catch

--- src/app/get/response.ts
+++ src/app/get/response.ts
@@ -6,15 +6,18 @@ import { getProjects } from "./getProjects";
 import { getAwards, getProfile, getTeams } from ".";
 
 const response = async (req: NextRequest) => {
+  const t = [];
   try{
-    
+    t.push("response");
     const db = await connectToDatabase();
+    t.push("db");
     const collection = db.collection("views");
     const views = await collection.findOne({ 
       date: {
         $gte: new Date(new Date().getTime() - 1000 * 60 * 60 * 24 * 7),
       },
     });
+    t.push("views");
   
     if (!views)  {
       const [projects, awards, teams, profile] = await Promise.all([
@@ -23,10 +26,12 @@ const response = async (req: NextRequest) => {
         getTeams(),
         getProfile(),
       ]);
+      t.push("getProjects");
       await collection.insertOne({ 
         date: new Date(),
         projects, awards, teams, profile
       });
+      t.push("insertOne");
       const res = NextResponse.json({ projects, awards, teams, profile });
       return res;
     }
@@ -36,8 +41,7 @@ const response = async (req: NextRequest) => {
     return res;
   }
   catch(e: any){
-    console.error(e.message);
-    return NextResponse.json([e.message], {
+    return NextResponse.json([e.message, t], {
       status: 500,
     });
   }
