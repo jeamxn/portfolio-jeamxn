455c8e19ac79288c8647846d6e1210db452187d7
jeamxn <admin@chicken-moo.com>
2025-03-22 13:34:47 +0900

    fix: catch

--- src/app/get/response.ts
+++ src/app/get/response.ts
@@ -6,32 +6,39 @@ import { getProjects } from "./getProjects";
 import { getAwards, getProfile, getTeams } from ".";
 
 const response = async (req: NextRequest) => {
-  const db = await connectToDatabase();
-  const collection = db.collection("views");
-  const views = await collection.findOne({ 
-    date: {
-      $gte: new Date(new Date().getTime() - 1000 * 60 * 60 * 24 * 7),
-    },
-  });
-
-  if (!views)  {
-    const [projects, awards, teams, profile] = await Promise.all([
-      getProjects(), 
-      getAwards(),
-      getTeams(),
-      getProfile(),
-    ]);
-    await collection.insertOne({ 
-      date: new Date(),
-      projects, awards, teams, profile
+  try{
+    
+    const db = await connectToDatabase();
+    const collection = db.collection("views");
+    const views = await collection.findOne({ 
+      date: {
+        $gte: new Date(new Date().getTime() - 1000 * 60 * 60 * 24 * 7),
+      },
     });
+  
+    if (!views)  {
+      const [projects, awards, teams, profile] = await Promise.all([
+        getProjects(), 
+        getAwards(),
+        getTeams(),
+        getProfile(),
+      ]);
+      await collection.insertOne({ 
+        date: new Date(),
+        projects, awards, teams, profile
+      });
+      const res = NextResponse.json({ projects, awards, teams, profile });
+      return res;
+    }
+  
+    const { projects, awards, teams, profile } = views;
     const res = NextResponse.json({ projects, awards, teams, profile });
     return res;
   }
-
-  const { projects, awards, teams, profile } = views;
-  const res = NextResponse.json({ projects, awards, teams, profile });
-  return res;
+  catch(e: any){
+    console.error(e.message);
+    return NextResponse.json([e.message]);
+  }
 };
 
 export default response;
